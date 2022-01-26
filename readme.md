924e857d9c090236a0823320f3c9f01d5dd40884
David Skelton <dskelton@cityofsydney.nsw.gov.au>
2022-01-27 10:08:31 +1100

    Initial commit

---
+++ readme.md
@@ -0,0 +1,1 @@
+[Tailwind UI in Svelte](https://imfeld.dev/notes/tailwind_in_svelte)
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,16 @@
+<script>
+import "../app.css";
+import Hero from "$lib/hero.svelte";
+import Footer from "$lib/footer.svelte";
+import Content from "$lib/content.svelte";
+import FeatureList from "$lib/feature-list.svelte";
+</script>
+
+
+<Hero />
+<main>
+  <slot />
+  <Content />
+  <FeatureList />
+</main>
+<Footer />
